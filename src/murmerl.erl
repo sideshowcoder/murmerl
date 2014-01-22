74e2e1a3972b594b9faa75bb9bc4d606dcec0c92
Philipp Fehre <philipp.fehre@googlemail.com>
2014-01-22 16:44:44 +0100

    add api and docs

--- src/murmerl.erl
+++ src/murmerl.erl
@@ -1,9 +1,9 @@
 -module(murmerl).
 
 -export([
-         murmur3_x86_32/2,
-         murmur3_x86_128/2,
-         murmur3_x64_128/2
+         murmur3_32/2,
+         murmur3_128/3,
+         murmur3_128/2
         ]).
 
 -on_load(init/0).
@@ -29,12 +29,75 @@ init() ->
     end,
     erlang:load_nif(SoName, 0).
 
+%% ===================================================================
+%% API
+%% ===================================================================
 
-murmur3_x86_32(_, _) -> exit(nif_library_no_loaded).
-murmur3_x86_128(_, _) -> exit(nif_library_no_loaded).
-murmur3_x64_128(_, _) -> exit(nif_library_no_loaded).
+% @doc murmur3_32/2 is a wrapper for the murmur3 C library by Peter Scott,
+% it takes a passed iolist and a seed value and runs the murmur3 hash on the
+% iolist the returned binary is the 32Bit hash value. The function is
+% implemented as a NIF as murmur3_x86_32.
+-spec murmur3_32(string(), integer()) -> binary().
+murmur3_32(Value, Seed) ->
+    murmur3_x86_32(Value, Seed).
 
+% @doc murmur3_128/2 is a wrapper for the murmur3 C library by Peter Scott, it
+% takes a passed iolist and a seed value and runs the murmur3 hash on the
+% iolist the returned binary is the 128Bit hash value. This function defaults
+% to the x64 optimised implementation of murmur3 with 128 Bit return.  The
+% function is implemented as a NIF as murmur3_x64_128.
+-spec murmur3_128(string(), integer()) -> binary().
+murmur3_128(Value, Seed) ->
+    murmur3_x64_128(Value, Seed).
+
+% @doc murmur3_128/3 is a wrapper for the murmur3 C library by Peter Scott, it
+% takes a passed iolist and a seed value and runs the murmur3 hash on the
+% iolist the returned binary is the 128Bit hash value. This function allows to
+% select either the x86 optimized version by passing x86 or the x64 optimized
+% version by passing x64 in either case it will return the 128 Bit hash.  The
+% function is implemented as a NIF as murmur3_x64_128 / murmur3_x86_128.
+-spec murmur3_128(string(), integer(), x64|x86) -> binary().
+murmur3_128(Value, Seed, x86) ->
+    murmur3_x86_128(Value, Seed);
+murmur3_128(Value, Seed, x64) ->
+    murmur3_x64_128(Value, Seed).
+
+%% ===================================================================
+%% NIF Fallbacks
+%% ===================================================================
+murmur3_x86_32(_, _) ->
+    erlang:nif_error({error, not_loaded}).
+
+murmur3_x86_128(_, _) ->
+    erlang:nif_error({error, not_loaded}).
+
+murmur3_x64_128(_, _) ->
+    erlang:nif_error({error, not_loaded}).
+
+%% ===================================================================
+%% EUnit tests
+%% ===================================================================
 -ifdef(TEST).
+
+murmur3_32_test() ->
+    Value = murmur3_32("foo", 23),
+    Other = murmur3_x86_32("foo", 23),
+    ?assert(Value == Other).
+
+murmur3_128_default_test() ->
+    Value = murmur3_128("foo", 23),
+    Other = murmur3_x64_128("foo", 23),
+    ?assert(Value == Other).
+
+murmur3_128_x86_version_test() ->
+    Value = murmur3_128("foo", 23, x86),
+    Other = murmur3_x86_128("foo", 23),
+    ?assert(Value == Other).
+
+murmur3_128_x64_version_test() ->
+    Value = murmur3_128("foo", 23, x64),
+    Other = murmur3_x64_128("foo", 23),
+    ?assert(Value == Other).
 
 murmur3_x86_32_test() ->
     Value = murmur3_x86_32("foo", 23),
