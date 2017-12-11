bebbf1913849666da3cf8a41a91522146fcc15bf
Louis-Philippe Gauthier <lpgauth@gmail.com>
2017-12-11 15:15:08 -0800

    Use enif_make_new_binary properly

--- c_src/murmur3_nif.c
+++ c_src/murmur3_nif.c
@@ -20,8 +20,10 @@ mk_error(ErlNifEnv *env, const char *msg) {
 
 extern ERL_NIF_TERM
 murmur3_x86_32(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-  ErlNifBinary ret, in;
+  ErlNifBinary in;
+  ERL_NIF_TERM ret;
   int seed;
+  unsigned char *hash;
 
   if (!enif_inspect_iolist_as_binary(env, argv[0], &in)) {
     return enif_make_badarg(env);
@@ -31,20 +33,18 @@ murmur3_x86_32(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     return enif_make_badarg(env);
   }
 
-  if (!enif_alloc_binary(4, &ret)) {
-    mk_error(env, "failed_to_alloc_bin");
-  }
-  memset(ret.data, 0, 4);
+  hash = enif_make_new_binary(env, 4, &ret);
+  MurmurHash3_x86_32(in.data, in.size, seed, hash);
 
-  MurmurHash3_x86_32(in.data, in.size, seed, ret.data);
-
-  return enif_make_binary(env, &ret);
+  return ret;
 }
 
 extern ERL_NIF_TERM
 murmur3_x86_128(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-  ErlNifBinary ret, in;
+  ErlNifBinary in;
+  ERL_NIF_TERM ret;
   int seed;
+  unsigned char *hash;
 
   if (!enif_inspect_iolist_as_binary(env, argv[0], &in)) {
     return enif_make_badarg(env);
@@ -54,20 +54,18 @@ murmur3_x86_128(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     return enif_make_badarg(env);
   }
 
-  if (!enif_alloc_binary(16, &ret)) {
-    mk_error(env, "failed_to_alloc_bin");
-  }
-  memset(ret.data, 0, 16);
+  hash = enif_make_new_binary(env, 16, &ret);
+  MurmurHash3_x64_128(in.data, in.size, seed, hash);
 
-  MurmurHash3_x64_128(in.data, in.size, seed, ret.data);
-
-  return enif_make_binary(env, &ret);
+  return ret;
 }
 
 extern ERL_NIF_TERM
 murmur3_x64_128(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
-  ErlNifBinary ret, in;
+  ErlNifBinary in;
+  ERL_NIF_TERM ret;
   int seed;
+  unsigned char *hash;
 
   if (!enif_inspect_iolist_as_binary(env, argv[0], &in)) {
     return enif_make_badarg(env);
@@ -77,15 +75,12 @@ murmur3_x64_128(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     return enif_make_badarg(env);
   }
 
-  if (!enif_alloc_binary(16, &ret)) {
-    mk_error(env, "failed_to_alloc_bin");
-  }
-  memset(ret.data, 0, 16);
+  hash = enif_make_new_binary(env, 16, &ret);
+  MurmurHash3_x64_128(in.data, in.size, seed, hash);
 
-  MurmurHash3_x86_128(in.data, in.size, seed, ret.data);
-
-  return enif_make_binary(env, &ret);
+  return ret;
 }
+
 static ErlNifFunc nif_funcs[] = {
   {"murmur3_x86_32", 2, murmur3_x86_32},
   {"murmur3_x86_128", 2, murmur3_x86_128},
